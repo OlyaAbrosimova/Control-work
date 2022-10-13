4af17ec0e32230ada9ac1c95f4f566f3ea08ffa8
Olya <abrosimovaon@yandex.ru>
2022-10-13 12:20:06 +0400

    Решение задачи

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array = new string[4] {"54", "456", "hello", "ok"};
+string[] newarray = new string[array.Length];
+
+NewArray(array, newarray);
+PrintArray(newarray);
+
+void NewArray(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
